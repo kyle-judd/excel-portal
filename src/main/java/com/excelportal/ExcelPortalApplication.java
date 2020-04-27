b4abb52bd790f67eef1e301d7e34a18b421c5d19
kyle-judd <juddkyle1408@gmail.com>
2020-04-27 16:13:13 -0400

    inital commit

---
+++ src/main/java/com/excelportal/ExcelPortalApplication.java
@@ -0,0 +1,13 @@
+package com.excelportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excelportal/ExcelPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
